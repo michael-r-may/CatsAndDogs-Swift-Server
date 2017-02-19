28f4b27cc6343f12542d710da84f3fa037db31b3
michael-r-may <michael_r_may@myfastmail.com>
2017-02-19 17:22:07 +0000

    Started adding a means to get the json schedule back

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,9 +2,18 @@
 import Glibc
 #endif
 
+// Inquiline provides a "Collection of utilities to aid use of the Nest Web Server Gateway Interface. Provides a RequestType and ResponseType implementation."
 import Inquiline
+
+// "Curassow is a Swift Nest HTTP Server. It uses the pre-fork worker model...It exposes a Nest-compatible interface for your application, allowing you to use Curassow with any Nest compatible web frameworks of your choice."
 import Curassow
 
-serve { _ in
-  return Response(.ok, contentType: "text/plain", content: "Cats And Dogs - Swift - Server Says Hello\n")
+serve { request in
+  switch request.path {
+    case "/schedule":
+      return Response(.ok, contentType: "application/json", content: "{\"hello\" : \"world\"}")
+
+    default:
+      return Response(.ok, contentType: "text/plain", content: "Cats And Dogs - Swift - Server Says Hello\n")
+  }
 }
