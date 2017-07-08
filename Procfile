4f877655e97819e7fcb908e043ed36cf486fe103
Romain Piel <romain.piel@gmail.com>
2017-07-08 15:02:50 +0100

    moving to Kitura

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: CatsAndDogs-Swift-Server --workers 5 --bind 0.0.0.0:$PORT
-lweb: ./.build/debug/CatsAndDogs-Swift-Server --workers 1 --bind 127.0.0.1:8080
+web: .build/release/CatsAndDogs-Swift-Server 
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,19 +1,19 @@
-#if os(Linux)
-import Glibc
-#endif
-
+import Kitura
 import Foundation
 
-import Inquiline
+let router = Router()
 
-import Curassow
-
-serve { request in
-  switch request.path {
-    case "/schedule":
-      return Response(.ok, contentType: "application/json", content: MCE4().toJSON())
-
-    default:
-      return Response(.ok, contentType: "text/plain", content: "Cats And Dogs - Swift - Server Says Hello\n")
-  }
+router.get("/") { _, response, next in
+    response.send("Cats And Dogs - Swift - Server Says Hello\n")
+    next()
 }
+
+router.get("/schedule") { _, response, next in
+    response.send(MCE4().toJSON())
+    next()
+}
+
+let port = Int(ProcessInfo.processInfo.environment["PORT"] ?? "8090") ?? 8090
+Kitura.addHTTPServer(onPort: port, with: router)
+
+Kitura.run()
